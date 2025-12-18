27458a91dac96e337b8827ccbebc79f83f2ca7ff
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2025-12-18 13:06:48 -0300

    Slipt sample_tags on packages

--- django_sample_components/templatetags/sample_tags/simple_alert.py
+++ django_sample_components/templatetags/sample_tags/simple_alert.py
@@ -1,40 +1,7 @@
-
-import time
-
-from django import template
-from django.utils.safestring import mark_safe, SafeString
 from django.template.loader import render_to_string
-
-# Create an instance of the template library
-register = template.Library()
+from django.utils.safestring import SafeString
 
 
-# Use the simple_tag decorator to register the function as a template tag
-@register.simple_tag
-def show_today_timestamp():
-    """
-    Returns the current Unix timestamp (seconds since epoch).
-    """
-    # time.time() returns the timestamp as a float
-    timestamp = int(time.time())
-
-    # mark_safe is optional here, but it's good practice if the output were raw HTML
-    return str(timestamp)
-
-
-# --- Example of how you would do it if the tag needed arguments ---
-@register.simple_tag
-def greeting(name):
-    """Returns a personalized greeting."""
-    return f"Hello, {name}!"
-
-
-@register.simple_block_tag
-def shout(content: SafeString, bg_color):
-    return mark_safe(f"<h2 style='text-transform:uppercase; background-color:{bg_color}'>{content}!!!!</h2>")
-
-
-@register.simple_block_tag
 def simple_alert(
     content: SafeString,
     type: str = 'info',
