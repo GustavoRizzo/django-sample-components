758edd172720dbd3d4e32e2a9070a95d4020e3c1
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2025-12-18 13:10:18 -0300

    Adding more tests

--- demo_project/kernel/tests/test_loadpage.py
+++ demo_project/kernel/tests/test_loadpage.py
@@ -6,6 +6,18 @@ class HomePageTests(SimpleTestCase):
     """Test class to verify the home page is accessible."""
     app_name = 'django_sample_components'
 
+    home_page_url = reverse(f"{app_name}:home")
+    greeting_page_url = reverse(f"{app_name}:greeting")
+    alert_page_url = reverse(f"{app_name}:alert")
+
     def test_home_page_status_code(self):
-        response = self.client.get(reverse(f"{self.app_name}:home"))
+        response = self.client.get(self.home_page_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_greeting_page_status_code(self):
+        response = self.client.get(self.greeting_page_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_alert_page_status_code(self):
+        response = self.client.get(self.alert_page_url)
         self.assertEqual(response.status_code, 200)
